f9d173efa2126a59eaeb54e9ac5c88d01a279c76
celulasupera-prog <celulasupera@gmail.com>
2026-06-05 12:20:21 -0300

    Refactor image addition logic for 'isFerias' condition
    Simplified image addition by removing margins for 'isFerias' case.

--- app.js
+++ app.js
@@ -2528,17 +2528,7 @@ Object.assign(printWrap.style, {
     const pageH = 297;
 
    if (isFerias) {
-  const marginX = 4;
-  const marginY = 4;
-
-  doc.addImage(
-    imgData,
-    'PNG',
-    marginX,
-    marginY,
-    210 - marginX * 2,
-    297 - marginY * 2
-  );
+  doc.addImage(imgData, 'PNG', 0, 0, 210, 297);
 } else {
   const marginX = 5;
   const marginY = 5;
